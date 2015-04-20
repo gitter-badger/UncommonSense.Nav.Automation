84b463276a090f987d11733e0b47d06f8b0f93ab
Jan Hoek <github@uncommonsense.nl>
2015-04-20 20:58:08 +0200

    Add Compile-NAVApplicationObject function

--- UncommonSense.Nav.Automation.psm1
+++ UncommonSense.Nav.Automation.psm1
@@ -195,3 +195,57 @@ function Export-NAVApplicationObject
     {
     }
 }
+
+<#
+.Synopsis
+   Compiles a NAV application object
+#>
+function Compile-NAVApplicationObject
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Specifies the type of server to connect to (native or Microsoft SQL Server)
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateSet('SQL', 'Native')]
+        [string]$DatabaseServerType = 'SQL',
+
+        # Specifies the name of the database from which you want to export.
+        [Parameter(Mandatory=$true,ValueFromPipeLineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$DatabaseName,
+
+        # Specifies the name of the SQL server instance to which the database
+        # you want to export from is attached. The default value is the default
+        # instance on the local host (.).
+        [Parameter(ValueFromPipeLineByPropertyName=$true)]
+        [string]$DatabaseServer = '.',
+
+        # Specifies the type of the object to export
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [ValidateSet('TableData','Table','Form','Report','Dataport','Codeunit','XMLport','MenuSuite','Page')]
+        [string]$Type,
+
+        # Specifies the ID of the object to export
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [int]$ID
+    )
+
+    Begin
+    {
+        $HelperLibraryFileName = Join-Path $PSScriptRoot Org.Edgerunner.Dynamics.Nav.CSide.dll
+        Add-Type -Path $HelperLibraryFileName
+    }
+    Process
+    {
+        # Through the pipeline, we may receive multiple NAV development clients, or multiple
+        # object types/IDs. Because of the former, we are getting the client below, not in 
+        # the Begin section.
+        Write-Verbose "Connecting to $DatabaseServerType server $DatabaseServer, database $DatabaseName"
+        $Client = [Org.Edgerunner.Dynamics.Nav.CSide.Client]::GetClient($DatabaseServerType, $($DatabaseServer.ToUpperInvariant()), $($DatabaseName.ToUpperInvariant()), $Null)
+        $Client.CompileObject([Org.Edgerunner.Dynamics.Nav.CSide.NavObjectType]$Type, $ID)
+    }
+    End
+    {
+    }
+}

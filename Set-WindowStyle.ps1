f22f82e9868bc07636431f9f889a1cb197a30f34
Jan Hoek <github@uncommonsense.nl>
2015-06-01 10:10:27 +0200

    Set-WindowStyle function

---
+++ Set-WindowStyle.ps1
@@ -0,0 +1,40 @@
+﻿# https://gist.github.com/jakeballard/11240204
+
+function Set-WindowStyle 
+{
+    param(
+        [Parameter()]
+        [ValidateSet('FORCEMINIMIZE', 'HIDE', 'MAXIMIZE', 'MINIMIZE', 'RESTORE', 
+                     'SHOW', 'SHOWDEFAULT', 'SHOWMAXIMIZED', 'SHOWMINIMIZED', 
+                     'SHOWMINNOACTIVE', 'SHOWNA', 'SHOWNOACTIVATE', 'SHOWNORMAL')]
+        $Style = 'SHOW',
+    
+        [Parameter(ValueFromPipeLineByPropertyName)]
+        $MainWindowHandle = (Get-Process -Id $pid).MainWindowHandle
+    )
+
+    $WindowStates = @{
+        'FORCEMINIMIZE'   = 11
+        'HIDE'            = 0
+        'MAXIMIZE'        = 3
+        'MINIMIZE'        = 6
+        'RESTORE'         = 9
+        'SHOW'            = 5
+        'SHOWDEFAULT'     = 10
+        'SHOWMAXIMIZED'   = 3
+        'SHOWMINIMIZED'   = 2
+        'SHOWMINNOACTIVE' = 7
+        'SHOWNA'          = 8
+        'SHOWNOACTIVATE'  = 4
+        'SHOWNORMAL'      = 1
+    }
+    
+    $Win32ShowWindowAsync = Add-Type `
+        -MemberDefinition '[DllImport("user32.dll")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' `
+        -Name Win32ShowWindowAsync `
+        -Namespace Win32Functions `
+        -PassThru
+    
+    $Win32ShowWindowAsync::ShowWindowAsync($MainWindowHandle, $WindowStates[$Style]) | Out-Null
+    Write-Verbose ("Set Window Style '{1} on '{0}'" -f $MainWindowHandle, $Style)
+}

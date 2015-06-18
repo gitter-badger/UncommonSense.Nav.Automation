0870a6087c31d993f1e3cb3e41524676e51910c9
Jan Hoek <github@uncommonsense.nl>
2015-06-18 13:57:13 +0200

    Testing with mutually exclusive dynamic params, like server name and database name for the native client

---
+++ Test-DynamicParams.ps1
@@ -0,0 +1,44 @@
+﻿function Test-DynamicParams
+{
+    [CmdletBinding()]
+    Param
+    (
+    )
+
+    DynamicParam
+    {
+        $ParameterDictionary = New-Object -Type System.Management.Automation.RuntimeDefinedParameterDictionary
+
+        if ((-not $PSBoundParameters.Server))
+        {
+            $ParameterDictionary.Add('Server', (New-DynamicParameter -ParameterName Server))            
+            $ParameterDictionary.Add('Database', (New-DynamicParameter -ParameterName Database))            
+        }    
+
+        return $ParameterDictionary 
+    }
+
+    Process
+    {
+        $PSBoundParameters
+    }
+}
+
+function New-DynamicParameter
+{
+    Param
+    (
+        [Parameter(Mandatory)]
+        [string]$ParameterName
+    )
+
+    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
+    $ParameterAttribute.Mandatory = $True
+
+    $AttributeCollection = New-Object -Type System.Collections.ObjectModel.Collection[System.Attribute]
+    $AttributeCollection.Add($ParameterAttribute)
+
+    New-Object -Type System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
+}
+
+Test-DynamicParams

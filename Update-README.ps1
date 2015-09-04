2e17c40ea2fb0b0245bdd9db2478cc4ab23672b9
Jan Hoek <github@uncommonsense.nl>
2015-09-04 15:07:52 +0200

    Add script for updating README

---
+++ Update-README.ps1
@@ -0,0 +1,14 @@
+﻿. C:\users\jhoek\GitHub\Scripts\Get-HelpAsMarkDown.ps1
+Set-Location C:\Users\jhoek\Documents\WindowsPowerShell\Modules\UncommonSense.Nav.Automation
+
+Import-Module UncommonSense.Nav.Automation -Force
+Get-Command `
+    -Module UncommonSense.Nav.Automation | `
+    Sort-Object -Property Noun,Verb | `
+        Get-HelpAsMarkDown `
+            -Title UncommonSense.Nav.Automation `
+            -Description 'PowerShell utils for Microsoft Dynamics NAV' `
+            -PrefacePath (Join-Path $Pwd PREFACE.md) `
+            -PostfacePath (JOin-Path $Pwd POSTFACE.md) |`
+                Out-File .\README.md #>
+

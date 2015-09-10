a7c719c582239f42399065b2d9dcb1f68036f7a5
Jan Hoek <github@uncommonsense.nl>
2015-09-10 18:10:44 +0200

    Get-NavDevelopmentClient and data files
    Get-NavDevelopmentClient retrieves available development environment
    executables

---
+++ Get-NavDevelopmentClient.new.ps1
@@ -0,0 +1,40 @@
+﻿function Get-NavDevelopmentClient
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory)]
+        [ValidateSet('Native','Sql')]
+        [string]$Type,
+
+        [Parameter(Mandatory,ParameterSetName='List')]
+        [Switch]$List,
+
+        [Parameter(Mandatory,ParameterSetName='Name',Position=0)]
+        [string]$Name
+    )
+
+    switch($Type)
+    {
+        'Native' { $ConfigsFileName = Join-Path $PSScriptRoot "fin.txt" } 
+        'Sql'    { $ConfigsFileName = Join-Path $PSScriptRoot "finsql.txt" }
+    }
+    
+    if (-not (Test-Path -Path $ConfigsFileName))
+    {
+        'Name,Path' | Out-File -FilePath $ConfigsFileName 
+    }
+    else
+    {
+        $Configs = Import-Csv -Path $ConfigsFileName -Delimiter ','
+
+        if ($Name)
+        {
+            $Configs | Where-Object -Property Name -Eq -Value $Name
+        }
+        else
+        {
+            $Configs
+        }
+    }
+}

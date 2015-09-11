dfc5466738d0ac7e85a6cbce00bcc5f375957944
Jan Hoek <github@uncommonsense.nl>
2015-09-11 14:11:25 +0200

    Introduced Stop-NAVDevelopmentClient

---
+++ Stop-NAVDevelopmentClient.ps1
@@ -0,0 +1,35 @@
+﻿function Stop-NAVDevelopmentClient
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory,ValueFromPipeLine,ValueFromPipeLineByPropertyName)]
+        [Org.Edgerunner.Dynamics.Nav.CSide.Client]$Client
+    )
+
+    Process
+    {
+        foreach($Item in $Client)
+        {
+            $Process = Get-Process -Name *fin* | Where-Object -Property MainWindowHandle -Eq $Item.WindowHandle
+
+            if ($Process)
+            {
+                Write-Verbose "NAV development client process ID is $($Process.Id)" 
+
+                if (-not $Process.CloseMainWindow())
+                {
+                    Write-Error -Message "Failed to stop NAV development client process."
+                }
+                else
+                {
+                    Write-Verbose -Message "Successfully stopped NAV development client process."
+                }
+            }
+            else
+            {
+                Write-Error -Message "Could not find process for NAV development client."
+            }
+        }
+    }
+}

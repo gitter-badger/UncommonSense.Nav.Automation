f8916c73f005160838a72afa66b6b9bbfff74222
Jan Hoek <github@uncommonsense.nl>
2015-09-11 14:11:13 +0200

    Isolated Start-NAVDevelopmentClient from Get-NAVDevelopmentClient

---
+++ Start-NAVDevelopmentClient.ps1
@@ -0,0 +1,48 @@
+﻿function Start-NAVDevelopmentClient
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory,ValueFromPipeLine,ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-Path $_ } )]
+        [string]$DevClientPath,
+
+        [Parameter(Mandatory)]
+        [ValidateSet('Native','Sql')]
+        [string]$DatabaseServerType,
+
+        [string]$DatabaseServerName,
+
+        [string]$DatabaseName,
+
+        # Controls how the development client window is displayed
+        [ValidateSet('Hidden', 'Maximized', 'Minimized', 'Normal')]
+        [string]$WindowStyle = 'Normal'
+    )
+
+    $Arguments = @()
+
+    if ($DatabaseServerName)
+    {
+        $Arguments += ('servername={0}' -f $DatabaseServerName)
+    }
+
+    if ($DatabaseName)
+    {
+        $Arguments += ('database={0}' -f $DatabaseName)
+    }
+
+    if ($ZupPath) 
+    { 
+        $Arguments += ('id={0}' -f $ZupPath)  
+    }
+
+    $ProcessStartInfo = New-Object -TypeName System.Diagnostics.ProcessStartInfo
+    $ProcessStartInfo.FileName = $DevClientPath
+    $ProcessStartInfo.WindowStyle = $WindowStyle
+    $ProcessStartInfo.Arguments = $Arguments -join ','
+
+    $Process = [System.Diagnostics.Process]::Start($ProcessStartInfo)
+
+    Start-Sleep -Seconds 1
+}

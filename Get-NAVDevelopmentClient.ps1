6169266c6b920a30f5871aea8fe6f8afde97dcd0
Jan Hoek <github@uncommonsense.nl>
2015-09-22 11:28:30 +0200

    Get-NAVDevelopmentClient can no longer be used to forcefully start clients; removed corresponding parameters

--- Get-NAVDevelopmentClient.ps1
+++ Get-NAVDevelopmentClient.ps1
@@ -10,7 +10,6 @@ function Get-NAVDevelopmentClient
     (
         # Filters running clients by server type
         [Parameter(ParameterSetName='Filters')]
-        [Parameter(Mandatory,ParameterSetName='Settings')]
         [ValidateSet('Native','Sql')]
         [string]$DatabaseServerType,
 
@@ -26,49 +25,10 @@ function Get-NAVDevelopmentClient
         [Parameter(ParameterSetName='Filters')]
         [Switch]$List,
 
-        # Opens the specified configuration if it is not running yet
-        [Parameter(ParameterSetName='Settings')]
-        [Switch]$Force,
-
-        [Parameter(ParameterSetName='Settings')]
-        [ValidateScript( { Test-Path $_ } )]
-        [string]$DevClientPath,
-
-        [Parameter(ParameterSetName='Settings')]
-        [string]$LiteralDatabaseServerName,
-
-        [Parameter(ParameterSetName='Settings')]
-        [string]$LiteralDatabaseName,
-
-        # Controls how the development client window is displayed
-        [Parameter(ParameterSetName='Settings')]
-        [ValidateSet('Hidden', 'Maximized', 'Minimized', 'Normal')]
-        [string]$WindowStyle = 'Normal'
     )
 
     Process
     {
-        $FilteredClients = Get-FilteredClients -DatabaseServerType $DatabaseServerType -DatabaseServerName $DatabaseServerName -DatabaseName $DatabaseName
-
-        if (-not $FilteredClients) 
-        {
-            if ($Force)
-            {
-                if (-not $LiteralDatabaseServerName)
-                {
-                    $LiteralDatabaseServerName = $DatabaseServerName
-                }
-
-                if (-not $LiteralDatabaseName)
-                {
-                    $LiteralDatabaseName = $DatabaseName
-                }
-
-                Start-NavDevelopmentClient -DevClientPath $DevClientPath -DatabaseServerType $DatabaseServerType -DatabaseServerName $LiteralDatabaseServerName -DatabaseName $LiteralDatabaseName -WindowStyle $WindowStyle
-            }
-        }
-        
-        $FilteredClients = Get-FilteredClients -DatabaseServerType $DatabaseServerType -DatabaseServerName $DatabaseServerName -DatabaseName $DatabaseName
 
         # List mode
         if ($List)

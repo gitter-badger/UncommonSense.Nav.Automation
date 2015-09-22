0aebdb6caa0115a09c3b9987d9f38ac19028c70c
Jan Hoek <github@uncommonsense.nl>
2015-09-22 15:46:30 +0200

    Add new demo script; FIXME: combine the two scripts

---
+++ Demo/Demo2.ps1
@@ -0,0 +1,23 @@
+﻿$SourceConfig = Get-NAVDatabaseConnectionConfig -Name CRONUSW12015
+$SourceClient = $SourceConfig | Start-NAVDevelopmentClient -PassThru -WindowStyle Minimized
+$SourceFolder = (New-Item (Join-Path $Home\Desktop Source) -ItemType Container -Force).FullName
+
+$Objects = $SourceClient | Get-NAVApplicationObjectInfo -TypeFilter 'Table|Page' -IDFilter '1..10' # | Out-GridView -PassThru
+
+if ($Objects)
+{
+    $TargetConfig = Get-NAVDatabaseConnectionConfig -Name CUSTOM
+    $TargetClient = $TargetConfig | Start-NAVDevelopmentClient -PassThru -WindowStyle Minimized
+    $TargetFolder = (New-Item (Join-Path $Home\Desktop Target) -ItemType Container -Force).FullName
+
+    $Objects | Export-NAVApplicationObject -Client $SourceClient.Client -Path $SourceFolder
+    $SourceClient.Client | Stop-NAVDevelopmentClient
+
+    # Wait for breakpoint file to be released before closing second client
+    Start-Sleep -Seconds 1
+
+    $Objects | Export-NAVApplicationObject -Client $TargetClient.Client -Path $TargetFolder
+    $TargetClient.Client | Stop-NAVDevelopmentClient
+
+    Start-Process -FilePath 'C:\Program Files\Araxis\Araxis Merge\Compare.exe' -ArgumentList $SourceFolder, $TargetFolder -WindowStyle Maximized
+}
